86303e9430cf1329fd4e8de10967880111f77bfb
Iago Silva Lima <47872956+IagoSilvaLima96@users.noreply.github.com>
2024-06-29 07:48:46 -0300

    Feat/change ride price (#2)
    * feat:change price normal fare
    
    * feat:change price overnight fare

--- ride/src/domain/fare/chain-of-responsibility/overnight-fare-calculator.handler.ts
+++ ride/src/domain/fare/chain-of-responsibility/overnight-fare-calculator.handler.ts
@@ -2,7 +2,7 @@ import segment from "../../ride/segment";
 import { FareCalculatorHandler } from "./fare-calculator.handler";
 
 export class OvernightFareCalculatorHandler extends FareCalculatorHandler {
-  TAX_FARE = 3.9;
+  TAX_FARE = 4.9;
   calculate(segment: segment): number {
     if (segment.isOvernight() && !segment.isSunday()) {
       return this.TAX_FARE * segment.distance;
--- ride/test/unit/ride.test.ts
+++ ride/test/unit/ride.test.ts
@@ -13,7 +13,7 @@ test("Deve fazer o cálculo do preço de uma corrida durante o dia", function ()
     -48.522234807851476,
     new Date("2021-03-01T10:00:00")
   );
-  expect(ride.calculate()).toBe(21);
+  expect(ride.calculate()).toBe(22);
 });
 
 test("Deve fazer o cálculo do preço de uma corrida durante a noite", function () {
@@ -28,7 +28,7 @@ test("Deve fazer o cálculo do preço de uma corrida durante a noite", function 
     -48.522234807851476,
     new Date("2021-03-01T23:00:00")
   );
-  expect(ride.calculate()).toBe(39);
+  expect(ride.calculate()).toBe(49);
 });
 
 test("Deve fazer o cálculo do preço de uma corrida no domingo de dia", function () {
